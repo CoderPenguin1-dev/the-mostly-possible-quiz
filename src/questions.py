d3f01a05c71ac6468c3b6654050c3d147fa8de50
CoderPenguin1 <coderpenguin1.dev@gmail.com>
2025-11-06 13:53:00 -0500

    Add the rest of the test questions

--- src/questions.py
+++ src/questions.py
@@ -1,7 +1,19 @@
 from question import Question
 
 question1 = Question("Can a match box?", 
-                     ["No but a tin can.", "Huh?", "One beat Mick Bison.", "Nah"], 1)
+                     ["No but a tin can.", "Huh?", "One beat Mick Bison.", "Nah"], 0)
 
 question2 = Question("How many chicken nuggets does it take to take to beat a rhino?", 
-                     ["All of them.", "0 they'll be gone soon :(.", "Just 1 dino nuggie :).", "π"], 2)
+                     ["All of them.", "0 they'll be gone soon :(.", "Just 1 dino nuggie :).", "π"], 1)
+
+question3 = Question("What did Billy do?",
+                     ["Do a sick kickflip!", "Commit unimaginable crimes.", "Scroll infront of an octopus.", "Become William."], 2)
+
+question4 = Question("What did Big Ben eat?",
+                     ["A pepper.", "15 pizzas.", "Hilton Hotel egg and cheese.", "Uhhh, chicken?"], 0)
+
+question5 = Question("If you break into a house and then into 3 more, where are you?",
+                     ["The fourth home.", "Solitary confinement.", "The living room.", "An apartment."], 3)
+
+question6 = Question("How many holes in a loaf of bread?",
+                     ["Too many to count.", "38.", "8.", "Zero... idiot."], 0)
