b384d78f1d6a2c36815ef96d9856fd518163787c
CoderPenguin1 <coderpenguin1.dev@gmail.com>
2025-11-11 10:43:22 -0500

    Implement writer logic

--- src/main.py
+++ src/main.py
@@ -12,28 +12,29 @@ question_writer = turtle.Turtle()
 question_writer.hideturtle()
 question_writer.penup()
 question_writer.goto(-100,100)
-question_writer.write(questions[0].question, font=fonts.question)
+
 answer1_writer = turtle.Turtle()
 answer1_writer.hideturtle()
 answer1_writer.penup()
 answer1_writer.goto(-90,50)
-answer1_writer.write("A. "+questions[0].answers[0],font=fonts.answers)
+
 answer2_writer = turtle.Turtle()
 answer2_writer.hideturtle()
 answer2_writer.penup()
 answer2_writer.goto(-90,0)
-answer2_writer.write("B. "+questions[0].answers[1],font=fonts.answers)
+
 answer3_writer = turtle.Turtle()
 answer3_writer.hideturtle()
 answer3_writer.penup()
 answer3_writer.goto(-90,-50)
-answer3_writer.write("C. "+questions[0].answers[2],font=fonts.answers)
+
 answer4_writer = turtle.Turtle()
 answer4_writer.hideturtle()
 answer4_writer.penup()
 answer4_writer.goto(-90,-100)
-answer4_writer.write("D. "+questions[0].answers[3],font=fonts.answers)
+
 gameover_writer = turtle.Turtle()
+
 wipe_drawer = turtle.Turtle()
 wipe_drawer.speed(0)
 wipe_drawer.pensize(30)
@@ -41,7 +42,11 @@ wipe_drawer.pencolor("red")
 
 
 def display_question(question_number : int):
-  pass
+  question_writer.write(questions[question_number].question, font=fonts.question)
+  answer1_writer.write("A) "+ questions[question_number].answers[0],font=fonts.answers)
+  answer2_writer.write("B) "+ questions[question_number].answers[1],font=fonts.answers)
+  answer3_writer.write("C) "+ questions[question_number].answers[2],font=fonts.answers)
+  answer3_writer.write("D) "+ questions[question_number].answers[3],font=fonts.answers)
 
 def display_game_over():
    pass
@@ -66,7 +71,6 @@ def display_wipe():
   wipe_drawer.clear()
 
 def main():
-  display_wipe()
   wn.mainloop()
 
 if __name__ == "__main__":
