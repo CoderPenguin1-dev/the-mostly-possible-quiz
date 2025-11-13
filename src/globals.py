fb5b301300980129faa32021532ab797fed45dfc
CoderPenguin1 <coderpenguin1.dev@gmail.com>
2025-11-13 14:14:56 -0500

    Add questions to question list

--- src/globals.py
+++ src/globals.py
@@ -1,5 +1,6 @@
 from questions import *
 from game_states import GameStates
-questions : list[Question] = [question1, question2, question3, question4, question5, question6]
+questions : list[Question] = [question1, question2, question3, question4, question5, question6,
+                              question7, question8, quesiton9, question10]
 question_amount : int = len(questions)
 game_state : GameStates = GameStates.MAIN_MENU
